1063badc8f490fb188fa2dd419cae826ac453f98
YorgiAlejandro <yorgidelriomarquez@gmail.com>
2024-01-21 14:33:17 -0500

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by BeMillionaire on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
